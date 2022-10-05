c5e2b05161ef654376165b81ae062e2dddb0c7e3
Sebastian Rolon <js@rolon.co>
2022-10-05 19:05:47 -0400

    can fully create vm with single script

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Repo for VM NVMe performance testing
 
-The objective of this repo is to run a single command to trigger all the configured test scenarios.
+The objective of this repo is to run as few commands as possible to trigger all the configured test scenarios.
 
-The execution flow is meant to be:
+### 1. Ansible host provisioning
 
+From the local machine, run ansible-playbook to get the server ready to start performing benchmarks:
+
+```commandline
+ansible-playbook playbook.yml -i hosts 
 ```
-Ansible Host Provisioning -> VM Setup Script -> cloud-init VM Provisioning -> Benchmarking Script
-```
-
-VMs are set up and benchmarking is run for them individually.
--- ansible/roles/host/files/run_vm.sh
+++ ansible/roles/host/files/run_vm.sh
@@ -3,7 +3,7 @@
 ## This script is meant to be run inside the discslab server
 ## we assume that spdk & multiprocess qemu have been cloned and built
 
-set -x
+set -e
 
 if [[ "${EUID}" -ne 0 ]]
   then echo "Please run as root"
@@ -24,27 +24,37 @@ if [[ ! -f "${vm_os_image}" ]]; then
   qemu-img create -f qcow2 "${vm_os_image}" 10G
 fi
 
+# the following is https://github.com/nutanix/libvfio-user/blob/master/docs/spdk.md
 if [[ "${vm_name}" == "vfio-user" ]]; then
   # give control of the nvme to vfio instead of the kernel
   # assign 24GB of hugepages
   HUGEMEM=24000 PCI_ALLOWED="0000:bc:00.0" ${spdk_path}/scripts/setup.sh
 
+  echo "### waiting for 12000 hugepages of 2048KiB to be allocated..."
+  until [[ $(awk '/^HugePages_Total:/ {print $2}' /proc/meminfo) -eq "12000" ]]; do
+    sleep 3
+  done
+
   # run the nvmeof target process (the one that listens to i/o requests) and pin it to 4 cores
   LD_LIBRARY_PATH="${spdk_path}/build/lib:${spdk_path}/dpdk/build/lib"  \
     ${spdk_path}/build/bin/nvmf_tgt -m '[32, 33, 34, 35]' &
 
+  echo "### waiting until the spdk process is ready..."
+  until ${spdk_path}/scripts/rpc.py framework_wait_init > /dev/null; do
+    sleep 3
+  done
+
   # remote procedure calls to the nvmf process to configure the bdev to talk to the physical nvme
   rm -f /var/run/{cntrl,bar0}
-  scripts/rpc.py nvmf_create_transport -t VFIOUSER
-	scripts/rpc.py bdev_nvme_attach_controller -b NVMe0 -t PCIe -a "0000:bc:00.0"
-	scripts/rpc.py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode0 -a -s SPDK0
-	scripts/rpc.py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode0 NVMe0n1
-	scripts/rpc.py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode0 -t VFIOUSER -a /var/run -s 0
+  ${spdk_path}/scripts/rpc.py nvmf_create_transport -t VFIOUSER
+  ${spdk_path}/scripts/rpc.py bdev_nvme_attach_controller -b NVMe0 -t PCIe -a "0000:bc:00.0"
+  ${spdk_path}/scripts/rpc.py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode0 -a -s SPDK0
+  ${spdk_path}/scripts/rpc.py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode0 NVMe0n1
+  ${spdk_path}/scripts/rpc.py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode0 -t VFIOUSER -a /var/run -s 0
 fi
 
 # run the vm
-vm_running=$(virsh list --all | grep "${vm_name}")
-if [[ -z "${vm_running}" ]]; then
-  echo "Creating VM"
-  virsh create "/nvme-fio/bench_server_config/ansible/roles/host/files/libvirt_xml/${vm_name}.xml"
+if ! virsh list --all --name | grep "${vm_name}"; then
+  echo "### creating VM..."
+  virsh create "/nvme-fio/bench_server_config/ansible/roles/host/files/libvirt_xml/qemu-${vm_name}.xml"
 fi
