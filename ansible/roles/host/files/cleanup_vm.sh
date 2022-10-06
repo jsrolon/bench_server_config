b88684a81a02bce475ae6f8fe114b6489bcadf83
Sebastian Rolon <js@rolon.co>
2022-10-06 16:01:48 -0400

    minor improvements to cleanup script

--- ansible/roles/host/files/cleanup_vm.sh
+++ ansible/roles/host/files/cleanup_vm.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ "${EUID}" -ne 0 ]]
   then echo "Please run as root"
+  exit 1
+fi
+
+vm_name="${1}"
+if [[ -z "${vm_name}" ]]; then
+  echo "Need vm name"
   exit 1
 fi
 
@@ -11,7 +19,7 @@ spdk_path="/nutanix-src/spdk"
 pkill --signal INT --oldest --full 'nvmf_tgt'
 
 # destroy vm
-pkill --signal INT -f 'qemu-vfio-user'
+pkill --signal INT --oldest --full "qemu-${vm_name}"
 
 # return nvme control to the kernel
 HUGEMEM=24000 PCI_ALLOWED="0000:bc:00.0" ${spdk_path}/scripts/setup.sh reset
